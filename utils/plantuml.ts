42123891cc241feb83c2a379fd035621ee800758
Kubantsev-VA <Kubantsev.V.A@sberbank.ru>
2020-08-26 08:39:21 +0300

    master
    create svg

--- utils/plantuml.ts
+++ utils/plantuml.ts
@@ -2,21 +2,34 @@ import { PlantUmlPipe } from 'plantuml-pipe'
 import { setImage, findImage } from 'utils/filesystem'
 
 const getImage = async (uml: string): Promise<string> => {
-  const image = ''
-  
+  let image = ''
+  let isDone = false
+
   const pipe = new PlantUmlPipe()
 
   pipe.out.on('data', (chunk: string) => {
-    image.concat(chunk)
+    image = String(chunk)
+    isDone = true
   })
 
   pipe.in.write(uml)
   pipe.in.end()
 
-  return image
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (isDone) {
+        console.log('image: ', image)
+        
+        resolve(image)
+        clearInterval(interval)
+      }
+    }, 100)
+  })
 }
 
 export const getWithSvg = async (content: string): Promise<string> => {
+  let result = ''
+
   const plantumls = (
     content.match(/```plantuml(\n)(.|\n)*`{3}(\n)/g) || 
     []
@@ -29,10 +42,8 @@ export const getWithSvg = async (content: string): Promise<string> => {
 
   for (const plantuml of plantumls) {
     const image = await getImage(plantuml)
-    console.log('image: ', image)
-    
-    content.replace(/```plantuml(\n)(.|\n)*`{3}(\n)/, image)
+    result = content.replace(/```plantuml(\n)(.|\n)*`{3}(\n)/, image)
   }
 
-  return content
+  return result
 }
